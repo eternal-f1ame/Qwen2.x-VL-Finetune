222de5c218dae74ca221a69f75bc66f06bdce8e3
Aaditya Baranwal <thunder.tanay.1907@gmail.com>
2025-05-10 10:19:18 +0000

    changed processor

--- src/train/train_sft.py
+++ src/train/train_sft.py
@@ -2,7 +2,7 @@ import os
 import torch
 from peft import LoraConfig, get_peft_model
 import ast
-from transformers import AutoProcessor, BitsAndBytesConfig, Qwen2VLForConditionalGeneration, HfArgumentParser, Qwen2_5_VLForConditionalGeneration
+from transformers import Qwen2_5_VLProcessor, BitsAndBytesConfig, Qwen2VLForConditionalGeneration, HfArgumentParser, Qwen2_5_VLForConditionalGeneration
 from train.trainer import QwenTrainer
 from train.data import make_supervised_data_module
 from train.params import DataArguments, ModelArguments, TrainingArguments
@@ -178,7 +178,7 @@ def train():
                 if "merger" in name:
                     param.requires_grad = True
 
-    processor = AutoProcessor.from_pretrained(model_args.model_id)
+    processor = Qwen2_5_VLProcessor.from_pretrained(model_args.model_id)
 
     # model.config.tokenizer_model_max_length = processor.tokenizer.model_max_length
 
